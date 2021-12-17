8ba60363fac851662a72bc6d3c6fd4f3acf96e46
GiaKhang <waoknight@gmail.com>
2021-12-17 20:57:17 +0700

    get don_hang and ct_dh by id_ttkh"

--- server/controller/ct_don_hang.js
+++ server/controller/ct_don_hang.js
@@ -1,101 +1,122 @@
-import { CT_DON_HANG_Model  } from "../models/CT_DON_HANG_Model.js"
+import {
+  CT_DON_HANG_Model,
+  Find_CTDH_By_ID_TTKH,
+} from "../models/CT_DON_HANG_Model.js";
 
 export const Get_CT_DHs = async (req, res) => {
-	/*
+  /*
 	Fetch all chi tiet don hang
 	:return: array 
 	*/
 
-	try {
-		const ct_dhs = await CT_DON_HANG_Model.find()
-			.populate({path: 'id_san_pham', select: "ten_sp"})
-			.populate('id_don_hang')
+  try {
+    const ct_dhs = await CT_DON_HANG_Model.find()
+      .populate({ path: "id_san_pham", select: "ten_sp" })
+      .populate("id_don_hang");
 
-		console.log('ct_dhs', ct_dhs)
-		res.status(200).json(ct_dhs)
-	} catch (err) {
-		res.status(500).json({ error: err })
-	}
-}
+    console.log("ct_dhs", ct_dhs);
+    res.status(200).json(ct_dhs);
+  } catch (err) {
+    res.status(500).json({ error: err });
+  }
+};
 
-export const Create_CT_DH = async (req, res)  => {
-	/*
+export const Create_CT_DH = async (req, res) => {
+  /*
 	Create ct don hang, this method will be called when
 	creating don hang in controller don_hang
 	:return: json
 	*/
 
-	try {
-		const new_ct_dh = req.body;
-		const ct_dh = new CT_DON_HANG_Model(new_ct_dh)
-		await ct_dh.save()
+  try {
+    const new_ct_dh = req.body;
+    const ct_dh = new CT_DON_HANG_Model(new_ct_dh);
+    await ct_dh.save();
 
-		res.status(200).json(ct_dh);
-	} catch(err) {
-		res.status(500).json({ error: err });
+    res.status(200).json(ct_dh);
+  } catch (err) {
+    res.status(500).json({ error: err });
     console.log(err);
-	}
-}
-
-
+  }
+};
 
 export const Get_CT_DH_By_Id = async (req, res) => {
-	/*
+  /*
 	Fetch  ct don hang by _id 
 	:return: json, null if not found
 	*/
 
-	try {
-		const ctdh_id = req.body._id
-		const ctdh = await CT_DON_HANG_Model.findById(ctdh_id)
-			.populate('id_san_pham')
-			.populate('id_don_hang')
-		console.log('ctdh', ctdh)
-		if (ctdh == null) {
-			console.log("Not found ctdh")
-		}
-		res.status(200).json(ctdh)
-	} catch(err) {
-		res.status(500).json({ error: err })
-	}
-}
+  try {
+    const ctdh_id = req.body._id;
+    const ctdh = await CT_DON_HANG_Model.findById(ctdh_id)
+      .populate("id_san_pham")
+      .populate("id_don_hang");
+    console.log("ctdh", ctdh);
+    if (ctdh == null) {
+      console.log("Not found ctdh");
+    }
+    res.status(200).json(ctdh);
+  } catch (err) {
+    res.status(500).json({ error: err });
+  }
+};
 
 export const Get_Don_Hang_By_DH_Id = async (req, res) => {
-	/*
+  /*
 	Fetch ctdh by don hang id
 	:return: array
 	*/
 
-	try {
-		const id_dh = req.body.id_don_hang
-		const ctdh_s = await CT_DON_HANG_Model.find({ id_don_hang: id_dh }).populate({path: "id_san_pham", select: "ten_sp"}).exec()
-		console.log('ctdh_s', ctdh_s)
-		if (ctdh_s.length == 0) {
-			console.log("Not found ctdh")
-		}
-		res.status(200).json(ctdh_s)
-	} catch(err) {
-		res.status(500).json({ error: err })
-	}
-}
+  try {
+    const id_dh = req.body.id_don_hang;
+    const ctdh_s = await CT_DON_HANG_Model.find({ id_don_hang: id_dh })
+      .populate({ path: "id_san_pham", select: "ten_sp" })
+      .exec();
+    console.log("ctdh_s", ctdh_s);
+    if (ctdh_s.length == 0) {
+      console.log("Not found ctdh");
+    }
+    res.status(200).json(ctdh_s);
+  } catch (err) {
+    res.status(500).json({ error: err });
+  }
+};
 
 export const Update_CTDH = async (req, res) => {
-	/*
+  /*
 	Update ct don hang
 	:return: json
 	*/
 
-	try {
-		const update_ctdh = req.body
+  try {
+    const update_ctdh = req.body;
 
-		const ctdh = await CT_DON_HANG_Model.findOneAndUpdate(
-			{ _id: update_ctdh._id }, update_ctdh, { new: true }
-		)
-		console.log(ctdh)
-		res.status(200).json(ctdh)	
+    const ctdh = await CT_DON_HANG_Model.findOneAndUpdate(
+      { _id: update_ctdh._id },
+      update_ctdh,
+      { new: true }
+    );
+    console.log(ctdh);
+    res.status(200).json(ctdh);
+  } catch (err) {
+    res.status(500).json({ error: err });
+    console.log(err);
+  }
+};
 
-	} catch(err) {
-		res.status(500).json({ error: err })
-		console.log(err)
-	}
-}
+export const Get_CTDH_By_TTKH = async (req, res) => {
+  /*
+	Get ctdh by id_ttkh
+
+	:return: array
+	*/
+
+  try {
+    var id_ttkh = req.body.id_ttkh;
+    var rs = await Find_CTDH_By_ID_TTKH(id_ttkh);
+    res.status(200).json(rs);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+};
--- server/routers/ct_don_hang.js
+++ server/routers/ct_don_hang.js
@@ -1,22 +1,31 @@
-import express from 'express'
-import { Get_CT_DHs, Create_CT_DH, Get_CT_DH_By_Id,
-	Get_Don_Hang_By_DH_Id, Update_CTDH } from '../controller/ct_don_hang.js'
+import express from "express";
+import {
+  Get_CT_DHs,
+  Create_CT_DH,
+  Get_CT_DH_By_Id,
+  Get_Don_Hang_By_DH_Id,
+  Update_CTDH,
+  Get_CTDH_By_TTKH,
+} from "../controller/ct_don_hang.js";
 
-const router = express.Router()
+const router = express.Router();
 
 // GET: /ct_dh/
-router.get('/', Get_CT_DHs)
+router.get("/", Get_CT_DHs);
 
 // POST: /ct_dh/
-router.post('/', Get_CT_DH_By_Id)
+router.post("/", Get_CT_DH_By_Id);
 
 // POST: /ct_dh/create
-router.post('/create', Create_CT_DH)
+router.post("/create", Create_CT_DH);
 
 // POST: /ct_dh/by_dh_id
-router.post('/by_dh_id', Get_Don_Hang_By_DH_Id)
+router.post("/by_dh_id", Get_Don_Hang_By_DH_Id);
 
 // POST: /ct_dh/update
-router.post('/update', Update_CTDH)
+router.post("/update", Update_CTDH);
 
-export default router
+// POST: /ct_dh/by_ttkh
+router.post("/by_ttkh", Get_CTDH_By_TTKH);
+
+export default router;
