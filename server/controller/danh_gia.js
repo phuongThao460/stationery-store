53256038697e18ca9aa890bad36f94b4c2fbc708
GiaKhang <waoknight@gmail.com>
2021-12-18 10:32:42 +0700

    add module danh_gia

---
+++ server/controller/danh_gia.js
@@ -0,0 +1,80 @@
+import {
+  Find_All_Danh_Gia_In_DB,
+  Find_Danh_Gia_By_ID,
+  Create,
+  Update,
+} from "../models/DANH_GIA_Model.js";
+
+// ==========================================
+//              FUNCTION DEFINITIONS
+// ==========================================
+
+export const Get_DanhGias = async (req, res) => {
+  /*
+	Fetch all danh_gia in db
+	:return: Array
+	*/
+
+  try {
+    const danh_gias = await Find_All_Danh_Gia_In_DB;
+    res.status(200).json(danh_gias);
+  } catch (err) {
+    res.status(500).json({ error: err });
+  }
+};
+
+export const Get_Danh_Gia_By_ID = async (req, res) => {
+  /*
+	Fetch danh_gia by _id
+
+	:return: json, null if not found anything
+	*/
+
+  try {
+    const id_danh_gia = req.body._id;
+    const danh_gia = await Find_Danh_Gia_By_ID(id_danh_gia);
+
+    res.status(200).json(danh_gia);
+  } catch (err) {
+    res.status(500).json({ error: err });
+  }
+};
+
+export const Create_DanhGia = async (req, res) => {
+  /*
+	Add new danh_gia then save to db
+	
+	required value to create new danh gia: 
+		id_ttkh, id_san_pham, noi_dung_danh_gia,
+    so_sao_danh_gia, tinh_trang
+
+  :return: json
+	*/
+
+  try {
+    var danh_gia = req.body;
+    new_danh_gia = await Create(danh_gia);
+
+    //console.log("voucher: ", voucher);
+    res.status(200).json(new_danh_gia);
+  } catch (err) {
+    res.status(500).json({ error: err });
+  }
+};
+
+export const Update_DanhGia = async (req, res) => {
+  /*
+  Update danh gia
+
+  :return: json
+  */
+
+  try {
+    var new_danh_gia = req.body;
+    const rs = await Update(new_danh_gia);
+
+    res.status(200).json(rs);
+  } catch (err) {
+    res.status(500).json({ error: err });
+  }
+};
---
+++ server/models/DANH_GIA_Model.js
@@ -0,0 +1,126 @@
+import mongoose from "mongoose";
+
+// ==========================================
+//              MODEL DEFINITIONS
+// ==========================================
+
+const schema = new mongoose.Schema(
+  {
+    id_tkkh: {
+      type: mongoose.Types.ObjectId,
+      required: true,
+      ref: "TAI_KHOAN_KHACH_HANG",
+    },
+    id_san_pham: {
+      type: mongoose.Types.ObjectId,
+      required: true,
+      ref: "SAN_PHAM",
+    },
+    noi_dung_danh_gia: {
+      type: String,
+      required: true,
+      default: "",
+    },
+    so_sao_danh_gia: {
+      type: Number,
+      required: true,
+      default: 0,
+      min: [0, "Must at least 0, got {VALUE}"],
+      max: [5, "Must at most 5, got { VALUE }"],
+    },
+    tinh_trang: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+export const DANH_GIA_Model = mongoose.model("DANH_GIA", schema);
+
+// ==========================================
+//              FUNCTION DEFINITIONS
+// ==========================================
+
+export const Set_Tinh_Trang_Danh_Gia = (danh_gia, status) => {
+  /*
+	Set or update tinh trang for danh_gia
+	*/
+
+  danh_gia.tinh_trang = status;
+  return danh_gia;
+};
+
+export const Find_All_Danh_Gia_In_DB = async () => {
+  /*
+  Find all danh gia in db
+
+  :return: array
+  */
+
+  try {
+    var danh_gias = await DANH_GIA_Model.find();
+    return danh_gias;
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+};
+
+export const Find_Danh_Gia_By_ID = async (id_danh_gia) => {
+  /*
+  Find danh gia by _id
+
+  :return: json
+  */
+
+  try {
+    var danh_gia = await DANH_GIA_Model.findById(id_danh_gia);
+    return danh_gia;
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+};
+
+export const Create = async (danh_gia) => {
+  /*
+  Create and save new danh gia to db
+
+  :return: json
+  */
+
+  try {
+    var danh_gia = new DANH_GIA_Model(danh_gia);
+    danh_gia = await danh_gia.save();
+
+    return danh_gia;
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+};
+
+export const Update = async (new_danh_gia) => {
+  /*
+  Update danh gia
+
+  :return: json
+  */
+
+  try {
+    var id_danh_gia = new_danh_gia._id;
+
+    var danh_gia = await DANH_GIA_Model.findOneAndUpdate(
+      { _id: id_danh_gia },
+      new_danh_gia,
+      { new: true }
+    );
+
+    return danh_gia;
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+};
