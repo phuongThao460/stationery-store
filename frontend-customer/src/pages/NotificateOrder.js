4b7cd6d9a77bd459da4e0ec17126941552f3ec33
phuongThao460 <blackcompany1998@gmail.com>
2021-12-18 08:00:18 +0700

    change font size, id paypal

--- frontend-customer/src/pages/NotificateOrder.js
+++ frontend-customer/src/pages/NotificateOrder.js
@@ -5,22 +5,21 @@ function NotificateOrder() {
 
   return (
     <div style={{ textAlign: "center", marginTop: "150px", fontSize: "20px" }}>
-      <b style={{ fontSize: "40px", marginBottom: "15px" }}>
+      <h1 style={{ marginBottom: "15px" }}>
         Thank you for your order
-      </b>
+      </h1>
       <div style={{marginTop: "20px"}}>
-        <p>
+        <p style={{fontSize: "30px"}}>
           As soon as your package is on its way, you will receive a delivery
           confirmation from us by email{" "}
         </p>
-        <p>
+        <p style={{fontSize: "30px"}}>
           Please be so kind to leave us a review and share your purchase with us
           on social media
         </p>
       </div>
       <div style={{marginTop: "30px"}}>
-      
-        <Link to="/" style={{color: "blue", textDecoration: "underline"}}><AiOutlineArrowLeft style={{paddingRight: "10px"}}/>Continue shopping</Link>
+        <Link to="/" style={{color: "blue", textDecoration: "underline", fontSize: "30px"}}><AiOutlineArrowLeft style={{paddingRight: "10px"}}/>Continue shopping</Link>
       </div>
     </div>
   );
